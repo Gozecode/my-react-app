d59d8333da1fdaaccae06e305135758b89bbe075
Gozecode <romeo.teutsch@gmail.com>
2024-12-12 16:26:52 +0100

    state

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./App.css";
 
 import PokemonCard from "./components/PokemonCard";
@@ -9,14 +11,46 @@ const pokemonList = [
 			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
 	},
 	{
+		name: "charmander",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+	},
+	{
+		name: "squirtle",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+	},
+	{
+		name: "pikachu",
+		imgSrc:
+			"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+	},
+	{
 		name: "mew",
 	},
 ];
 
 function App() {
+	const [pokemonIndex, setPokemonIndex] = useState(0);
+	const pickNext = () => {
+		setPokemonIndex(pokemonIndex + 1);
+	};
+	const pickPreview = () => {
+		setPokemonIndex(pokemonIndex - 1);
+	};
 	return (
 		<>
-			<PokemonCard pokemon={pokemonList[0]} />
+			<PokemonCard pokemon={pokemonList[pokemonIndex]} />
+			{pokemonIndex > 0 && (
+				<button type="button" onClick={pickPreview}>
+					Précedent
+				</button>
+			)}
+			{pokemonIndex < pokemonList.length - 1 && (
+				<button type="button" onClick={pickNext}>
+					Suivant
+				</button>
+			)}
 		</>
 	);
 }
